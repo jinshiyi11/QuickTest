fbbf2489949c1ebc698ffcdaf1dba3f61416a93a
shuaiweican <49662990@qq.com>
2014-10-16 17:36:25 +0800

    1.quickx测试项目

---
+++ debug_win.bat
@@ -0,0 +1,25 @@
+@echo off
+
+set DIR=%~dp0
+
+echo %QUICK_V3_ROOT%
+
+set BIN=%QUICK_V3_ROOT%quick\player\proj.win32\bin\player3.exe
+rem debug
+set BIN=D:\open\quickv3\quick\player\proj.win32\Debug\player3.exe
+echo %BIN%
+
+if not exist %BIN% set BIN=%QUICK_V3_ROOT%quick\player\proj.win32\Release\player3.exe
+if not exist %BIN% set BIN=%QUICK_V3_ROOT%quick\player\proj.win32\Debug\player3.exe
+
+set ARG=-debugger-codeide -workdir %DIR%
+set SIZE=-portrait
+
+%BIN% %ARG% %SIZE%
+
+echo ""
+echo "------------------------------------------------------"
+echo ""
+echo ""
+echo ""
+pause
